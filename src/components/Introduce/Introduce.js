877745ac9ab4f97e24fcb8b95e1bc0bf69847c94
kuror0lucifer <kovalishin.2014@mail.ru>
2023-11-16 18:37:26 +0200

    adaptive 425px works screen

--- src/components/Introduce/Introduce.js
+++ src/components/Introduce/Introduce.js
@@ -10,7 +10,7 @@ export default function Introduce() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </h1>
           <p className="intro__text">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
